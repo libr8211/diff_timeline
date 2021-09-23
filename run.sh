1eaa367a160f6f59f4694631523478b0b1e6b4e6
Lindsey Joe Broadwell <libr8211@colorado.edu>
2021-09-23 11:40:36 -0600

    Ran nextflow on RNA-seq data and compared each timepoint lncMYH7b KD vs Ctl

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name=differentiation
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=lindsey.broadwell@colorado.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=30:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+pwd; hostname; date
+echo "You've requested $SLURM_CPUS_ON_NODE core."
+module load singularity/3.1.1
+nextflow run nf-core/rnaseq -r 1.4.2 \
+-resume \
+-profile singularity \
+--reads 'fastq/*{_1,_2}.fq.gz' \
+--fasta ../../genomes/Homo_sapiens/Gencode/v33/GRCh38.p13.genome.fa \
+--gtf util/gencode.v33.annotation.gtf \
+--pseudo_aligner salmon \
+--gencode \
+-c nextflow.config
+
+date
